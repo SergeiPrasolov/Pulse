8fe14b334cfc89f46bc2d156f103ded67f4aa5b8
SergeiPrasolov <prasol468@gmail.com>
2020-05-17 11:04:26 +0400

    Закончена секция табов

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 $(document).ready(function(){
     $('.carousel__inner').slick({
       speed: 1200,
@@ -15,6 +16,42 @@ $(document).ready(function(){
         }
       ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    /* $('.catalog-item__link').each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+
+    $('.catalog-item__list_back').each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    }); */
+    // Два закомментированных блока заменены функцией:
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+      });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__list_back');
   });
  
  
